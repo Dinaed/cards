054ac7de9f68b961baf00362eccf78c7336fb22c
Dinaed <Dinaedandr@gmail.com>
2022-01-08 10:01:47 +0200

    fin

---
+++ script/script.js
@@ -0,0 +1,14 @@
+const slides = document.querySelectorAll('.slide');
+
+slides.forEach(item => {
+  item.addEventListener('click', ()=> {
+    clearActive();
+    item.classList.add('active');
+  })
+})
+
+function clearActive() {
+  slides.forEach(item => {
+    item.classList.remove('active');
+  });
+}
